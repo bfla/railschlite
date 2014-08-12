5a640d604535e4084208812d57883d7415ff09c9
bfla <bfla@umich.edu>
2014-08-12 11:18:06 -0400

    installed friendly_ids and created their migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811163656) do
+ActiveRecord::Schema.define(version: 20140812151547) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -137,6 +137,19 @@ ActiveRecord::Schema.define(version: 20140811163656) do
 
   add_index "destinations", ["slug"], name: "index_destinations_on_slug"
   add_index "destinations", ["state_id"], name: "index_destinations_on_state_id"
+
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "states", force: true do |t|
     t.string   "name"
