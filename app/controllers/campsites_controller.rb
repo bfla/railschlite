cc76161b7486a67fbc7f814ff6e7bd538d467d7c
bfla <bfla@umich.edu>
2014-08-13 13:49:08 -0400

    fixed up destination#show&#index and state#index

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -5,7 +5,7 @@ class CampsitesController < ApplicationController
   # GET /campsites
   # GET /campsites.json
   def index
-    @campsites = Campsite.page(params[:page]).per(100)
+    @campsites = Campsite.all.page(params[:page]).per(100)
   end
 
   # GET /campsites/1
